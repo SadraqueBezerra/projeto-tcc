6a3a857f21870689699eaccbd6c2d9f0881e9f2c
Sadraque Bezerra <sadraque.bezerra23@gmail.com>
2021-11-05 17:00:27 -0300

    Project created

---
+++ backend/src/main/java/com/projetotg/iamedicina/IamedicinaApplication.java
@@ -0,0 +1,13 @@
+package com.projetotg.iamedicina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamedicinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamedicinaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetotg/iamedicina/IamedicinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetotg.iamedicina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamedicinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
